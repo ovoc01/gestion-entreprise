452e0420b82d71e9278f517569825698c2be1889
ovoc01 <spotifymirindra@gmail.com>
2023-09-28 09:11:35 +0300

    init project

---
+++ spring-server/src/main/java/com/gestion/entreprise/EntrepriseApplication.java
@@ -0,0 +1,13 @@
+package com.gestion.entreprise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntrepriseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntrepriseApplication.class, args);
+	}
+
+}
